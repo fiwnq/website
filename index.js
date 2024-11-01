71514ec748083166ddc18b7d0b8760f1258cfd19
fiwnq <landgrafj752@gmail.com>
2024-11-01 03:16:05 -0500

    ok maybe debugged

--- index.js
+++ index.js
@@ -2,40 +2,36 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5001
 
-const clientId = process.env.SPOTIFY_CLIENT_ID;
-const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
-const refreshToken = process.env.SPOTIFY_REFRESH_TOKEN;
-let accessToken = refreshToken;
+var client_id = process.env.SPOTIFY_CLIENT_ID;
+var client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+
+var redirect_uri = 'https://jland.dev/callback';
 
 async function refreshAccessToken() {
-  const authOptions = {
-      method: 'POST',
-      headers: {
-          'Authorization': 'Basic ' + Buffer.from(clientId + ':' + clientSecret).toString('base64'),
-          'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: new URLSearchParams({
-          grant_type: 'refresh_token',
-          refresh_token: refreshToken
-      })
+  var refresh_token = localStorage.getItem('access_token');
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+    },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
   };
 
-  try {
-      const response = await fetch('https://accounts.spotify.com/api/token', authOptions);
-      const data = await response.json();
-      console.log(process.env.SPOTIFY_CLIENT_ID);
-      console.log(process.env.SPOTIFY_CLIENT_SECRET);
-      console.log(process.env.SPOTIFY_REFRESH_TOKEN);
-
-      if (response.ok) {
-            accessToken = data.access_token;
-            console.log('Access token refreshed:', accessToken);
-        } else {
-            console.error('Error refreshing access token:', data);
-        }
-  } catch (error) {
-      console.error('Error refreshing access token:', error);
-  }
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var access_token = body.access_token,
+          refresh_token = body.refresh_token || refresh_token;
+      res.send({
+        'access_token': access_token,
+        'refresh_token': refresh_token
+      });
+    }
+  });
 }
 
 express()
@@ -61,7 +57,50 @@ express()
       console.error('Error fetching recently played tracks:', error);
       res.status(500).send('Internal Server Error');
     }
-  }) 
+  })
+
+  .get('/login', function(req, res) {
+
+    var state = generateRandomString(16);
+    var scope = 'user-read-private user-read-currently-playing user-top-read user-read-recently-played';
+  
+    res.redirect('https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
+        response_type: 'code',
+        client_id: client_id,
+        scope: scope,
+        redirect_uri: redirect_uri,
+        state: state
+      }));
+  })
+  
+  get('/callback', function(req, res) {
+
+    var code = req.query.code || null;
+    var state = req.query.state || null;
+  
+    if (state === null) {
+      res.redirect('/#' +
+        querystring.stringify({
+          error: 'state_mismatch'
+        }));
+    } else {
+      var authOptions = {
+        url: 'https://accounts.spotify.com/api/token',
+        form: {
+          code: code,
+          redirect_uri: redirect_uri,
+          grant_type: 'authorization_code'
+        },
+        headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret).toString('base64'))
+        },
+        json: true
+      };
+    }
+  })
+
   .listen(PORT, () => { 
     console.log(`Listening on ${ PORT }`);
     refreshAccessToken();
